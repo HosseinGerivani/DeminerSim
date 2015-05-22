00328e8e8708286f3d847aa1560845fefb5485db
HosseinGerivani <Hossein.Gerivani@gmail.com>
2015-05-23 01:44:15 +0430

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,38 @@
+
+public class Data {
+	private float x1;
+	private float y1;
+	private float x2;
+	private float y2;
+	private int number;
+	public void setX1(float x){
+		x1 = x;
+	}
+	public void setY1(float x){
+		y1 = x;
+	}
+	public void setX2(float x){
+		x2 = x;
+	}
+	public void setY2(float x){
+		y2 = x;
+	}
+	public void setNumber(int x){
+		number = x;
+	}
+	public float getX1() {
+		return x1;
+	}
+	public float getY1() {
+		return y1;
+	}
+	public float getX2() {
+		return x2;
+	}
+	public float getY2() {
+		return y2;
+	}
+	public int getNumber() {
+		return number;
+	}
+}
